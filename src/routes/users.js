6ca504acde6d45f36568fe17595ba08305f62b05
hauntedbae <rima.sakalauskaite4121@gmail.com>
2022-09-20 21:54:05 +0300

    Node.js exam

--- src/routes/users.js
+++ src/routes/users.js
@@ -16,29 +16,29 @@ router.get("/", async (req, res) => {
   }
 });
 
-// router.get("/:name", async (req, res) => {
-//   try {
-//     const con = await mysql.createConnection(dbconfig);
-//     const [response] = await con.execute(`SELECT id, name FROM users_db;`);
-//     await con.end();
-//     res.send(response);
-//   } catch (e) {
-//     res.status(400).send({ error: "Error" });
-//   }
-// });
+router.get("/:name", async (req, res) => {
+  try {
+    const con = await mysql.createConnection(dbconfig);
+    const [response] = await con.execute(`SELECT id, name FROM users_db;`);
+    await con.end();
+    res.send(response);
+  } catch (e) {
+    res.status(400).send({ error: "Error" });
+  }
+});
 
-// router.get("/:email", async (req, res) => {
-//   try {
-//     const con = await mysql.createConnection(dbconfig);
-//     const [response] = await con.execute(
-//       "SELECT id, name, email FROM users_db;"
-//     );
-//     await con.end();
-//     res.send(response);
-//   } catch (e) {
-//     res.status(400).send({ error: "Error" });
-//   }
-// });
+router.get("/:email", async (req, res) => {
+  try {
+    const con = await mysql.createConnection(dbconfig);
+    const [response] = await con.execute(
+      "SELECT id, name, email FROM users_db;"
+    );
+    await con.end();
+    res.send(response);
+  } catch (e) {
+    res.status(400).send({ error: "Error" });
+  }
+});
 
 router.get("/:address", async (req, res) => {
   try {
